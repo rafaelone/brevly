ffd179010d475cb48750d8c586f983b79a74d3b3
rafael sergio <rafa.sergio.02@live.com>
2025-04-13 22:39:48 -0300

    feat: upload csv

---
+++ server/src/app/usecases/get-csv-url-usecase.ts
@@ -0,0 +1,69 @@
+import { db, pg } from "@/db";
+import { schema } from "@/db/schemas";
+
+import { desc } from "drizzle-orm";
+import { stringify } from "csv-stringify";
+
+import type { StorageProvider } from "@/storage/storage";
+import { PassThrough, Transform } from "node:stream";
+
+import { pipeline } from "node:stream/promises";
+import { uuidv7 } from "uuidv7";
+
+export class GetCsvUrlUseCase {
+	constructor(private storage: StorageProvider) {}
+
+	async execute() {
+		const { sql, params } = db
+			.select({
+				id: schema.links.id,
+				original_link: schema.links.original_link,
+				short_link: schema.links.short_link,
+				access_count: schema.links.access_count,
+			})
+			.from(schema.links)
+			.orderBy(desc(schema.links.createdAt))
+			.toSQL();
+
+		const cursor = pg.unsafe(sql, params as string[]).cursor(1);
+
+		const csv = stringify({
+			delimiter: ",",
+			header: true,
+			columns: [
+				{ key: "id", header: "ID" },
+				{ key: "original_link", header: "Original URL" },
+				{ key: "short_link", header: "Short URL" },
+				{ key: "access_count", header: "Access Count" },
+			],
+		});
+
+		const uploadToStorageStream = new PassThrough();
+
+		const convertToCSVPipeline = pipeline(
+			cursor,
+			new Transform({
+				objectMode: true,
+				transform(chunks: unknown[], encoding, callback) {
+					for (const chunk of chunks) {
+						console.log(chunk);
+						this.push(chunk);
+					}
+					callback();
+				},
+			}),
+			csv,
+			uploadToStorageStream,
+		);
+
+		await convertToCSVPipeline;
+
+		const { url } = await this.storage.uploadFileAsStream({
+			fileName: `${uuidv7()}.csv`,
+			contentType: "text/csv",
+			stream: uploadToStorageStream,
+		});
+
+		return { url };
+	}
+}
---
+++ server/src/routes/get-csv-url.ts
@@ -0,0 +1,44 @@
+import { GetCsvUrlUseCase } from "@/app/usecases/get-csv-url-usecase";
+import { S3StorageProvier } from "@/storage/providers/s3";
+import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { z } from "zod";
+
+export const getCSVUrl: FastifyPluginAsyncZod = async (server) => {
+	server.get(
+		"/links/csv",
+		{
+			schema: {
+				summary: "Get a CSV Url",
+				tags: ["links"],
+
+				response: {
+					201: z.object({ url: z.string() }),
+					400: z.object({
+						message: z.string(),
+					}),
+				},
+			},
+		},
+		async (request, reply) => {
+			// const result = await getCsvUrlUseCase();
+
+			const storageProvider = new S3StorageProvier();
+			const response = new GetCsvUrlUseCase(storageProvider);
+
+			const { url } = await response.execute();
+
+			await reply.status(201).send({ url });
+
+			// if (isRight(result)) {
+			// 	return reply.status(200).send({ url: result.right.url });
+			// }
+
+			// const error = unwrapEither(result);
+
+			// switch (error.name) {
+			// 	case "BadRequest":
+			// 		return reply.status(400).send({ message: error.message });
+			// }
+		},
+	);
+};
